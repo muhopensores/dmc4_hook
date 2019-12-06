a65e6d6dbff460e7bd9bcddfb39fa2210f260255
Siyan <gsssiyan@gmail.com>
2019-12-06 14:02:18 +0000

    Added more comments

--- src/dmc4_hook/naked.h
+++ src/dmc4_hook/naked.h
@@ -178,7 +178,7 @@ _declspec(naked) void selectiveCancels_proc(void)
 			cancellableecstasy:
 			cmp [g_ecstasyCancelEnable],0x1	// If Gui is ticked,
 			je cancellable						// make the move cancellable
-			jmp originalcode					// if not, don't make it cancellable
+			jmp originalcode						// if not, don't make it cancellable
 
 			cancellableargument:
 			cmp [g_argumentCancelEnable],0x1
@@ -213,7 +213,7 @@ _declspec(naked) void selectiveCancels_proc(void)
 		cancellable:
 			mov dword ptr [esi+0x8C],0x02	// only movs to [esi+8C] after filtering out anything that doesn't have [esi+13C],FFFFFFFF and [esi+144],FFFFFFFF
 												// a change of cmps would allow for different types of cancels such as cancelling an animation with walking or another attack
-		originalcode:							// buffers are also used around this area - the je a few bytes down used to be an inconvenience
+		originalcode:								// buffers are also used around this area - the je a few bytes down used to be an inconvenience
 			mov edi,0x00000008				// originalcode has nothing to do with our newmem, just a convenient jmp point so always run
 			jmp dword ptr [_selectiveCancelsContinue]
     }
@@ -317,7 +317,7 @@ _declspec(naked) void trackingFullHouse_proc(void)
     }
 }
 
-_declspec(naked) void timerAlloc_proc(void)
+_declspec(naked) void timerAlloc_proc(void) // This + the next 3 are what make up down trick
 {
     _asm {
 		// timer
@@ -392,8 +392,8 @@ _declspec(naked) void backForward_proc(void)
 			cmp byte ptr [g_trickDownEnable],0
 			je honeycombcompare
 
-			cmp [timerMem],0x41200000			//=10
-			jl originalcode
+			cmp [timerMem],0x41200000			//=10	// If timer is less than 10, don't reset. With this, timer is only reset once per backforward.
+			jl originalcode									// 10 is over before trick finishes, so no worry about trick down not being available
 			cmp al,0x3
 			je resettimer
 
@@ -412,7 +412,7 @@ _declspec(naked) void backForward_proc(void)
 
 		originalcode:
 			mov [ebp+0x00],al
-			jmp dword ptr [_backForwardContinue]
+			jmp dword ptr [_backForwardContinue]// next opcode was jmp x so rather than returning I do that
     }
 }
 
@@ -422,12 +422,12 @@ _declspec(naked) void trickDown_proc(void)
 			cmp byte ptr [g_trickDownEnable], 0
 			je originalcode
 
-			cmp dword ptr [timerMem],0x42480000	//=50 //0x41a00000 //=20
+			cmp dword ptr [timerMem],0x42480000	//=50 //0x41a00000 //=20	// if timer is less than x, put x in y axis momentum
 			jl trickdownstart
 			jmp originalcode
 
 		trickdownstart:
-			movss xmm2,[downFloat]
+			movss xmm2,[downFloat]				// currently (float)-200. This may change, as when you're high enough trick won't reach the floor
 
 		originalcode:
 			movss [esi+0x00000EC4],xmm2
@@ -441,8 +441,8 @@ _declspec(naked) void floorTouch_proc(void)
 			cmp byte ptr [g_trickDownEnable], 0
 			je originalcode
 
-			cmp dword ptr [timerMem],0x42a00000	//=80
-			jl skipcode
+			cmp dword ptr [timerMem],0x42a00000	//=80	// if timer is less than x, skip the height adjust at the end of trick
+			jl skipcode										// this has to be higher than the start to account for how long trick takes to start up
 
 		originalcode:
 			movss [esi+0x00000EC4],xmm2
