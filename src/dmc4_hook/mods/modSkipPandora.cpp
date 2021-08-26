f66a1960159b6434d30c57da4c217e26a0fcb358
Mstislav Kapustka <awesome065@yandex.com>
2021-08-26 17:01:21 +0300

    fix skip pandora crashing securom exe

--- src/dmc4_hook/mods/modSkipPandora.cpp
+++ src/dmc4_hook/mods/modSkipPandora.cpp
@@ -2,63 +2,70 @@
 #include "modSkipPandora.hpp"
 
 #if 1
+static bool hooked{ false };
 
 bool SkipPandora::modEnabled{ false };
 uintptr_t SkipPandora::_skipPandoraContinue{ NULL };
 
 SkipPandora::SkipPandora()
 {
-    // onInitialize();
+	// onInitialize();
 }
 
 naked void skipPandora_proc(void)
 {
-    _asm {
+	_asm {
 		cmp byte ptr [SkipPandora::modEnabled], 0
 		je originalcode
 
-        cmp eax, 8
-        je skiptoei
-        jmp originalcode
+		cmp eax, 8
+		je skiptoei
+		jmp originalcode
 
-        skiptoei:
-        mov [edi+30h], 9
-        mov al, 01
-        jmp dword ptr [SkipPandora::_skipPandoraContinue]
+		skiptoei:
+		mov [edi+30h], 9
+			mov al, 01
+			jmp dword ptr [SkipPandora::_skipPandoraContinue]
 
-    originalcode:
-        mov [edi+30h], eax
-        mov al, 01
-		jmp dword ptr [SkipPandora::_skipPandoraContinue]
-    }
+			originalcode:
+			mov [edi+30h], eax
+			mov al, 01
+			jmp dword ptr [SkipPandora::_skipPandoraContinue]
+	}
 }
 
 std::optional<std::string> SkipPandora::onInitialize()
 {
-    if (!install_hook_offset(0x3B122F, hook, &skipPandora_proc, &SkipPandora::_skipPandoraContinue, 5)) // breaks 28mb exe
-    {
-        HL_LOG_ERR("Failed to init SkipPandora mod\n");
-        return "Failed to init SkipPandora mod";
-    }
+	// bad fix for crashing on securom exe
+	const char bytes[5] = { 0x89, 0x47, 0x30, 0xB0, 0x01 };
+	if (memcmp((void*)0x7B122F, bytes, sizeof(bytes))) {
+		hooked = false;
+		return Mod::onInitialize();
+	}
+	hooked = true;
+	if (!install_hook_offset(0x3B122F, hook, &skipPandora_proc, &SkipPandora::_skipPandoraContinue, 5)) // breaks 28mb exe
+	{
+		HL_LOG_ERR("Failed to init SkipPandora mod\n");
+		return "Failed to init SkipPandora mod";
+	}
 
-    return Mod::onInitialize();
+	return Mod::onInitialize();
 }
 
 void SkipPandora::onGUIframe()
 {
-    // from main.cpp
-    // line 907 -> main->getMods()->onDrawUI("NoClip"_hash);
-    ImGui::Checkbox("Skip Pandora", &modEnabled);
+	if (!hooked) { return; }
+	ImGui::Checkbox("Skip Pandora", &modEnabled);
 }
 
 void SkipPandora::onConfigLoad(const utils::Config& cfg)
 {
-    modEnabled = cfg.get<bool>("skip_pandora").value_or(false);
+	modEnabled = cfg.get<bool>("skip_pandora").value_or(false);
 };
 
 void SkipPandora::onConfigSave(utils::Config& cfg)
 {
-    cfg.set<bool>("skip_pandora", modEnabled);
+	cfg.set<bool>("skip_pandora", modEnabled);
 };
 
 #endif
