9aa5ebe550492c0d919b8a0d56de8e757b696565
SSSiyan <gsssiyan@gmail.com>
2025-05-07 01:54:28 +0100

    edit shadow res in case it was causing tex crash

--- src/mods/ShadowResolution.cpp
+++ src/mods/ShadowResolution.cpp
@@ -5,14 +5,20 @@ bool ShadowResolution::mod_enabled = false;
 uintptr_t ShadowResolution::jmp_ret1 = NULL;
 
 void ShadowResolution::toggle1(bool enable) {
-    if (!enable) { // reset mid gameplay
+    if (enable) { // reset mid gameplay
+        install_patch_offset(0x67D3B9, patch1, "\x08", 1); // patch cmp eax, 400 to cmp eax, 800
+        install_patch_offset(0x67D3C0, patch2, "\x08", 1); // patch mov eax, 400 to mov eax, 800
+    }
+    else {
+        patch1.reset();
+        patch2.reset();
         sUnit* sUnit = devil4_sdk::get_sUnit();
         if (!sUnit) { return; };
         if (!sUnit->shadow) { return; };
         sUnit->shadow->resolution = 0;
     }
 }
-
+/*
 static uintptr_t detour1_jmp_je = 0xA7D3E3;
 naked void detour1() {
     _asm {
@@ -28,7 +34,7 @@ naked void detour1() {
             jmp [detour1_jmp_je]
     }
 }
-
+*/
 // void on_frame(fmilliseconds& dt) {}
 
 void ShadowResolution::on_gui_frame(int display) {
@@ -41,10 +47,10 @@ void ShadowResolution::on_gui_frame(int display) {
 // bool on_message(HWND wnd, UINT message, WPARAM wParam, LPARAM lParam) {}
 
 std::optional<std::string> ShadowResolution::on_initialize() {
-    if (!install_hook_offset(0x67D3C9, hook1, &detour1, &jmp_ret1, 8)) {
+    /*if (!install_hook_offset(0x67D3C9, hook1, &detour1, &jmp_ret1, 8)) {
         spdlog::error("Failed to init ShadowResolution mod 1\n");
         return "Failed to init ShadowResolution mod 1";
-	}
+	}*/
 
     return Mod::on_initialize();
 }
