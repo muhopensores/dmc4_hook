9c3745edf8ae26e3cff4f474390a7c99f9e9d699
muhopensores <58393267+muhopensores@users.noreply.github.com>
2026-01-06 12:16:43 +0300

    get rid of Windows.h include in Patch.hpp

--- src/utility/Patch.hpp
+++ src/utility/Patch.hpp
@@ -5,11 +5,10 @@
 #include <vector>
 #include <optional>
 
-#include <Windows.h>
-
 class Patch {
 public:
     using Ptr = std::unique_ptr<Patch>;
+    using DWORD = unsigned long;
     static Ptr create(uintptr_t addr, const std::vector<int16_t>& b, bool should_enable = true);
     static Patch* create_raw(uintptr_t addr, const std::vector<int16_t>& b, bool should_enable);
     static Ptr create_nop(uintptr_t addr, uint32_t length, bool should_enable = true);
