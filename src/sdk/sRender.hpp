ff77c90504e0e77f35e8ba3e65f0b1a2f7718920
muhopensores <58393267+muhopensores@users.noreply.github.com>
2026-02-01 15:58:12 +0300

    add frame number to sdk/sRender.hpp

--- src/sdk/sRender.hpp
+++ src/sdk/sRender.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "cSystem.hpp"
-#include "MtMath.hpp"
 
 struct IDirect3DDevice9;
 
 class sRender : public cSystem {
 public:
-    char              pad_0020[20]; // 0x0020
+    char pad_0024[16]; //0x0024
     IDirect3DDevice9* mp_device; // 0x0034
-    char              pad_0038[12]; // 0x0038
+    char pad_0038[12]; //0x0038
     Vector2i          screenRes; // 0x0044
-}; // Size: 0x004C
-static_assert(sizeof(sRender) == 0x004C);
+    char pad_004C[88292]; //0x004C
+    uint32_t          mFrameNum; //0x15930
+}; //Size: 0x15934
+static_assert(sizeof(sRender) == 0x15930);
+
