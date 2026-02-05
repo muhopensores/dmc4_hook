d2d821f4f5403fe3943ed026fe8bf1d244632202
muhopensores <58393267+muhopensores@users.noreply.github.com>
2026-02-05 11:34:01 +0300

    fix sdk/sRender.hpp misalignment

--- src/sdk/sRender.hpp
+++ src/sdk/sRender.hpp
@@ -4,14 +4,20 @@
 
 struct IDirect3DDevice9;
 
-class sRender : public cSystem {
+class sRender : public cSystem
+{
 public:
-    char pad_0024[16]; //0x0024
-    IDirect3DDevice9* mp_device; // 0x0034
+    char pad_0020[20]; //0x0020
+    class ID3DDevice9 *mp_device; //0x0034
     char pad_0038[12]; //0x0038
-    Vector2i          screenRes; // 0x0044
+    Vector2i screenRes; //0x0044
     char pad_004C[88292]; //0x004C
-    uint32_t          mFrameNum; //0x15930
+    uint32_t mFrameNum; //0x15930
 }; //Size: 0x15934
-static_assert(sizeof(sRender) == 0x15930);
-
+static_assert(sizeof(sRender) == 0x15934);
+static_assert(offsetof(sRender, pad_0020) == 0x0020);
+static_assert(offsetof(sRender, mp_device) == 0x0034);
+static_assert(offsetof(sRender, pad_0038) == 0x0038);
+static_assert(offsetof(sRender, screenRes) == 0x0044);
+static_assert(offsetof(sRender, pad_004C) == 0x004C);
+static_assert(offsetof(sRender, mFrameNum) == 0x15930);
