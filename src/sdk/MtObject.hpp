2ae01ae1e81893394b8189ad6ff33583603436e6
SSSiyan <58395132+SSSiyan@users.noreply.github.com>
2026-04-28 22:59:23 +0100

    temp remove stuff from mtobject because i dont understand
    this fixes stand punch rush

--- src/sdk/MtObject.hpp
+++ src/sdk/MtObject.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 class MtDTI;
-
+/*
 class MtObject {
 public:
 
@@ -17,5 +17,15 @@ public:
     virtual void   create_property(void* prop) {};             // 0x0C
     virtual MtDTI* get_dti() { return (MtDTI*)0x00E5C5A8; };   // 0x10
 
+}; // Size: 0x0004
+static_assert(sizeof(MtObject) == 0x04);
+*/
+class MtObject {
+public:
+    virtual void vec_del_dtor(unsigned int i) {};            // 0x00
+    virtual void create_ui(void* prop) {};                   // 0x04
+    virtual bool is_enable_instance() { return 1; };         // 0x08
+    virtual void create_property(void* prop) {};             // 0x0C
+    virtual MtDTI* get_dti() { return (MtDTI*)0x00E5C5A8; }; // 0x10
 }; // Size: 0x0004
 static_assert(sizeof(MtObject) == 0x04);
